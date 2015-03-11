6da2607718e8a9f1cdeaab8f153d3f38336f1f75
Dmitry Khomutov <poisoncorpsee@gmail.com>
2015-03-11 23:01:35 +0600

    Added functions contains, cut, cutWords with tests

--- src/Strings.php
+++ src/Strings.php
@@ -13,20 +13,64 @@ namespace Nameless\Utilities\Strings;
 /**
  * @param string $haystack
  * @param string $needle
+ * @param string $encoding
  *
  * @return boolean
  */
-function startWith($haystack, $needle) {
-    return substr($haystack, 0, mb_strlen($needle, 'UTF-8')) === $needle;
+function startWith($haystack, $needle, $encoding = 'UTF-8') {
+    return ($needle === mb_substr($haystack, 0, mb_strlen($needle, $encoding), $encoding));
 }
 
 /**
  * @param string $haystack
  * @param string $needle
+ * @param string $encoding
  *
  * @return boolean
  */
-function endWith($haystack, $needle) {
-    return substr($haystack, -mb_strlen($needle, 'UTF-8')) === $needle;
+function endWith($haystack, $needle, $encoding = 'UTF-8') {
+    return ($needle === mb_substr($haystack, -mb_strlen($needle, $encoding), null, $encoding));
+}
+
+/**
+ * @param string $haystack
+ * @param string $needle
+ * @param string $encoding
+ *
+ * @return boolean
+ */
+function contains($haystack, $needle, $encoding = 'UTF-8') {
+    return mb_strpos($haystack, $needle, null, $encoding) !== false;
+}
+
+/**
+ * @param string  $string
+ * @param integer $limit
+ * @param string  $append
+ * @param string  $encoding
+ *
+ * @return string
+ */
+function cut($string, $limit = 25, $append = '...', $encoding = 'UTF-8') {
+    if (mb_strlen($string, $encoding) <= $limit) {
+        return $string;
+    }
+    return rtrim(mb_substr($string, 0, $limit, $encoding)) . $append;
+}
+
+/**
+ * @param string  $string
+ * @param integer $limit
+ * @param string  $append
+ * @param string  $encoding
+ *
+ * @return string
+ */
+function cutWords($string, $limit = 100, $append = '...', $encoding = 'UTF-8') {
+    preg_match('/^\s*+(?:\S++\s*+){1,' . $limit . '}/u', $string, $matches);
+    if (!isset($matches[0]) || mb_strlen($string, $encoding) === mb_strlen($matches[0], $encoding)) {
+        return $string;
+    }
+    return rtrim($matches[0]) . $append;
 }
 
--- tests/src/StringsTest.php
+++ tests/src/StringsTest.php
@@ -7,10 +7,30 @@ class StringsTest extends \PHPUnit_Framework_TestCase
     public function testStartWith()
     {
         $this->assertTrue(\Nameless\Utilities\Strings\startWith('example', 'exa'));
+        $this->assertTrue(\Nameless\Utilities\Strings\startWith('пример', 'пр'));
     }
     
     public function testEndWith()
     {
-        $this->assertTrue(\Nameless\Utilities\Strings\endWith('example', 'xample'));
+        $this->assertTrue(\Nameless\Utilities\Strings\endWith('example', 'ample'));
+        $this->assertTrue(\Nameless\Utilities\Strings\endWith('пример', 'мер'));
+    }
+
+    public function testContains()
+    {
+        $this->assertTrue(\Nameless\Utilities\Strings\contains('example', 'xampl'));
+        $this->assertTrue(\Nameless\Utilities\Strings\contains('пример', 'им'));
+    }
+
+    public function testCut()
+    {
+        $this->assertEquals(\Nameless\Utilities\Strings\cut('example', 6), 'exampl...');
+        $this->assertEquals(\Nameless\Utilities\Strings\cut('пример', 3), 'при...');
+    }
+
+    public function testCutWords()
+    {
+        $this->assertEquals(\Nameless\Utilities\Strings\cutWords('simple example', 1), 'simple...');
+        $this->assertEquals(\Nameless\Utilities\Strings\cutWords('очень простой пример', 2), 'очень простой...');
     }
 }
