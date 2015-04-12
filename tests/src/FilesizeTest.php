f9aac524f8cd876a46e4f089af6e7bdbf26dd814
Dmitry Khomutov <poisoncorpsee@gmail.com>
2015-04-12 13:11:51 +0600

    Fixed filesize test

--- tests/src/FilesizeTest.php
+++ tests/src/FilesizeTest.php
@@ -10,12 +10,12 @@ class FilesizeTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(\Nameless\Utilities\Filesize\humanize(1024, 0), '1KB');
         $this->assertEquals(\Nameless\Utilities\Filesize\humanize(1000000000), '953.67MB');
         $this->assertEquals(\Nameless\Utilities\Filesize\humanize(1000000000, 0), '954MB');
-        $this->assertEquals(\Nameless\Utilities\Filesize\humanize(1000000000, 4), '954MB');
+        $this->assertEquals(\Nameless\Utilities\Filesize\humanize(1000000000, 4), '953.6743MB');
     }
 
     public function testUnhumanize()
     {
-        $this->assertEquals(\Nameless\Utilities\Filesize\unhumanize('1.00KB'), 1024);
+        $this->assertEquals(\Nameless\Utilities\Filesize\unhumanize('1KB'), 1024);
         $this->assertEquals(\Nameless\Utilities\Filesize\unhumanize('953.67MB'), 999995474);
         $this->assertEquals(\Nameless\Utilities\Filesize\unhumanize('954MB'), 1000341504);
     }
