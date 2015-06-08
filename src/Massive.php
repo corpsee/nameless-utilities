751621116aff8e64d3a9ed50a0573f92a0634cf9
Dmitry Khomutov <poisoncorpsee@gmail.com>
2015-06-09 00:30:47 +0600

    Added Massive::toString function + tests

---
+++ src/Massive.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Nameless utilities package
+ *
+ * @copyright Copyright 2015, Corpsee.
+ * @license   https://github.com/corpsee/nameless-utilities/blob/master/LICENSE
+ * @link      https://github.com/corpsee/nameles-utilities
+ */
+
+namespace Nameless\Utilities\Massive;
+
+/**
+ * @param array  $array
+ * @param string $delimiter
+ *
+ * @return string
+ */
+function toString(array $array, $delimiter = ', ') {
+    if (!$array) {
+        return '';
+    }
+
+    foreach ($array as $key => $value) {
+        if (!$value) {
+            unset($array[$key]);
+        }
+    }
+
+    return implode($delimiter, $array);
+}
