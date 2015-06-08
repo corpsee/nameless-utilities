9f9597e58c01a53c28a49d2d0e052c65f1b326ec
Dmitry Khomutov <poisoncorpsee@gmail.com>
2015-06-08 23:34:05 +0600

    Fixed tests for PHPUnit 4.6.*

--- tests/src/FilesizeTest.php
+++ tests/src/FilesizeTest.php
@@ -6,17 +6,17 @@ class FilesizeTest extends \PHPUnit_Framework_TestCase
 {
     public function testHumanize()
     {
-        $this->assertEquals(\Nameless\Utilities\Filesize\humanize(512), '512.00B');
-        $this->assertEquals(\Nameless\Utilities\Filesize\humanize(1024, 0), '1KB');
-        $this->assertEquals(\Nameless\Utilities\Filesize\humanize(1000000000), '953.67MB');
-        $this->assertEquals(\Nameless\Utilities\Filesize\humanize(1000000000, 0), '954MB');
-        $this->assertEquals(\Nameless\Utilities\Filesize\humanize(1000000000, 4), '953.6743MB');
+        self::assertEquals(\Nameless\Utilities\Filesize\humanize(512), '512.00B');
+        self::assertEquals(\Nameless\Utilities\Filesize\humanize(1024, 0), '1KB');
+        self::assertEquals(\Nameless\Utilities\Filesize\humanize(1000000000), '953.67MB');
+        self::assertEquals(\Nameless\Utilities\Filesize\humanize(1000000000, 0), '954MB');
+        self::assertEquals(\Nameless\Utilities\Filesize\humanize(1000000000, 4), '953.6743MB');
     }
 
     public function testUnhumanize()
     {
-        $this->assertEquals(\Nameless\Utilities\Filesize\unhumanize('1KB'), 1024);
-        $this->assertEquals(\Nameless\Utilities\Filesize\unhumanize('953.67MB'), 999995474);
-        $this->assertEquals(\Nameless\Utilities\Filesize\unhumanize('954MB'), 1000341504);
+        self::assertEquals(\Nameless\Utilities\Filesize\unhumanize('1KB'), 1024);
+        self::assertEquals(\Nameless\Utilities\Filesize\unhumanize('953.67MB'), 999995474);
+        self::assertEquals(\Nameless\Utilities\Filesize\unhumanize('954MB'), 1000341504);
     }
 }
--- tests/src/StringsTest.php
+++ tests/src/StringsTest.php
@@ -6,43 +6,43 @@ class StringsTest extends \PHPUnit_Framework_TestCase
 {
     public function testStartWith()
     {
-        $this->assertTrue(\Nameless\Utilities\Strings\startWith('example', 'exa'));
-        $this->assertTrue(\Nameless\Utilities\Strings\startWith('пример', 'пр'));
+        self::assertTrue(\Nameless\Utilities\Strings\startWith('example', 'exa'));
+        self::assertTrue(\Nameless\Utilities\Strings\startWith('пример', 'пр'));
     }
     
     public function testEndWith()
     {
-        $this->assertTrue(\Nameless\Utilities\Strings\endWith('example', 'ample'));
-        $this->assertTrue(\Nameless\Utilities\Strings\endWith('пример', 'мер'));
+        self::assertTrue(\Nameless\Utilities\Strings\endWith('example', 'ample'));
+        self::assertTrue(\Nameless\Utilities\Strings\endWith('пример', 'мер'));
     }
 
     public function testContains()
     {
-        $this->assertTrue(\Nameless\Utilities\Strings\contains('example', 'xampl'));
-        $this->assertTrue(\Nameless\Utilities\Strings\contains('пример', 'им'));
+        self::assertTrue(\Nameless\Utilities\Strings\contains('example', 'xampl'));
+        self::assertTrue(\Nameless\Utilities\Strings\contains('пример', 'им'));
     }
 
     public function testCut()
     {
-        $this->assertEquals(\Nameless\Utilities\Strings\cut('example', 6), 'exampl...');
-        $this->assertEquals(\Nameless\Utilities\Strings\cut('пример', 3), 'при...');
+        self::assertEquals(\Nameless\Utilities\Strings\cut('example', 6), 'exampl...');
+        self::assertEquals(\Nameless\Utilities\Strings\cut('пример', 3), 'при...');
     }
 
     public function testCutWords()
     {
-        $this->assertEquals(\Nameless\Utilities\Strings\cutWords('simple example', 1), 'simple...');
-        $this->assertEquals(\Nameless\Utilities\Strings\cutWords('очень простой пример', 2), 'очень простой...');
+        self::assertEquals(\Nameless\Utilities\Strings\cutWords('simple example', 1), 'simple...');
+        self::assertEquals(\Nameless\Utilities\Strings\cutWords('очень простой пример', 2), 'очень простой...');
     }
     
     public function testTransliterate()
     {
-        $this->assertEquals(\Nameless\Utilities\Strings\transliterate('очень простой пример'), 'ochen prostoj primer');
-        $this->assertEquals(\Nameless\Utilities\Strings\transliterate('velmi jednoduchý příklad'), 'velmi jednoduchy priklad');
+        self::assertEquals(\Nameless\Utilities\Strings\transliterate('очень простой пример'), 'ochen prostoj primer');
+        self::assertEquals(\Nameless\Utilities\Strings\transliterate('velmi jednoduchý příklad'), 'velmi jednoduchy priklad');
     }
     
     public function testStandardize()
     {
-        $this->assertEquals(\Nameless\Utilities\Strings\standardize('очень простой   Пример', '-'), 'ochen-prostoj-primer');
-        $this->assertEquals(\Nameless\Utilities\Strings\standardize(' Velmi:;+ Jednoduchý___příklad  '), 'velmi_jednoduchy_priklad');
+        self::assertEquals(\Nameless\Utilities\Strings\standardize('очень простой   Пример', '-'), 'ochen-prostoj-primer');
+        self::assertEquals(\Nameless\Utilities\Strings\standardize(' Velmi:;+ Jednoduchý___příklad  '), 'velmi_jednoduchy_priklad');
     }
 }
