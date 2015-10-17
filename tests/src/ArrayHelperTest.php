62a3a745679c7add05f709cbb2366210bd181c3d
Dmitry Khomutov <poisoncorpsee@gmail.com>
2015-10-17 17:35:53 +0600

    Added ArrayHelper::get method + test

--- tests/src/ArrayHelperTest.php
+++ tests/src/ArrayHelperTest.php
@@ -12,4 +12,15 @@ class ArrayHelperTest extends \PHPUnit_Framework_TestCase
         self::assertEquals(ArrayHelper::toString([]), '');
         self::assertEquals(ArrayHelper::toString(['one', 'two', ''], ':'), 'one:two');
     }
+
+    public function testGet()
+    {
+        self::assertEquals(ArrayHelper::get(['one' => 1, 'two' => 2, 'three' => 3], 'one'), 1);
+        self::assertEquals(ArrayHelper::get(['one' => 1, 'two' => 2, 'three' => 3], 'four'), null);
+        self::assertEquals(ArrayHelper::get(['one' => 1, 'two' => 2, 'three' => 3], 'four', 4), 4);
+
+        self::assertEquals(ArrayHelper::get([0 => 'one', 1 => 'two', 2 => 'three'], 0), 'one');
+        self::assertEquals(ArrayHelper::get([0 => 'one', 1 => 'two', 2 => 'three'], 3), null);
+        self::assertEquals(ArrayHelper::get([0 => 'one', 1 => 'two', 2 => 'three'], 3, 4), 4);
+    }
 }
